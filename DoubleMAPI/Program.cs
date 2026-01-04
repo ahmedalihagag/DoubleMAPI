beed1dcef9b948fe29fa64d1e27215cf8b138c85
ahmedalihagag <ahmedalihagag1@gmail.com>
2026-01-04 06:07:12 +0200

    - Configuring Serilog - Removing unnecessary files

--- DoubleMAPI/Program.cs
+++ DoubleMAPI/Program.cs
@@ -1,16 +1,15 @@
-using BLL.Interfaces;
-using BLL.Services;
 using DAL.Data;
 using DAL.Entities;
 using DAL.Interfaces;
 using DAL.Repositories;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Serilog
+// Configure Serilog early so startup logs are captured
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .Enrich.FromLogContext()
@@ -18,16 +17,19 @@ Log.Logger = new LoggerConfiguration()
     .Enrich.WithThreadId()
     .CreateLogger();
 
-builder.Host.UseSerilog();
+// Hook Serilog into the generic host and ensure it reads configuration as well
+builder.Host.UseSerilog((hostingContext, services, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(hostingContext.Configuration)
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .Enrich.WithThreadId();
+});
 
 // Register Unit of Work (BLL will use this)
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-// Register Business Logic Services
-builder.Services.AddScoped<ICourseService, CourseService>();
-builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
-builder.Services.AddScoped<IQuizService, QuizService>();
-builder.Services.AddScoped<INotificationService, NotificationService>();
 
 
 // Add services to the container.
@@ -42,18 +44,58 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    Log.Information("Starting web host");
+
+    var app = builder.Build();
+
+
+    // logs HTTP requests
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.MessageTemplate =
+            "Handled {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+    });
+
+    //Exception Handling Middleware
+    app.UseExceptionHandler(errorApp =>
+    {
+        errorApp.Run(async context =>
+        {
+            var exception = context.Features
+                .Get<IExceptionHandlerFeature>()?.Error;
+
+            Log.Error(exception, "Unhandled exception");
+
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = "Unexpected error"
+            });
+        });
+    });
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
